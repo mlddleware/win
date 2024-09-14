e6a10c3c45638eb993c3944ce15b306e38283307
middleware <130544221+mlddleware@users.noreply.github.com>
2024-09-14 19:24:25 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
 import os
 
 app = Flask(__name__)
+CORS(app)  # Разрешаем все запросы с любого источника
 
 # Получаем порт и параметры подключения из переменных окружения
 port = int(os.environ.get("PORT", 5000))
